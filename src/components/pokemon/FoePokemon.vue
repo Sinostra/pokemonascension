066a43b575be152b46ad8b905bcd38d604b3cd66
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2022-07-29 18:57:03 +0200

    mise en place sélection d'un pokémon

--- src/components/pokemon/FoePokemon.vue
+++ src/components/pokemon/FoePokemon.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pokemon-wrapper" :style="foePosition">
+    <div class="pokemon-wrapper" :style="foePosition" :class="animClass">
       <div class="size-wrapper" :style="{'width': $store.state.pokedex.constantDex[id]['size'] + '%'}">
         <div class="bottom-wrapper" @click.stop="onClick()">
           <div class="healthBar-infos-wrapper">
@@ -8,7 +8,7 @@
               <div class="healthAmount" :style="getFontSize()">{{currentHealth}} / {{maxHealth}} </div>
             </div>
           </div>
-          <img :src="spritePath" :class="animClass">
+          <img :src="spritePath" :class="spriteClass">
         </div>
       </div>
   </div>
@@ -31,6 +31,11 @@ export default class FoePokemon extends Pokemon {
   get foePosition(): string {
     const currentBackground: string = this.$store.state.battle.backgroundUsed
     return `left: ${this.$store.state.backgrounds.slots[currentBackground]['foes'][this.index]['left']}%; bottom: ${this.$store.state.backgrounds.slots[currentBackground]['foes'][this.index]['bottom']}%;`
+  }
+
+  get spriteClass(): string {
+    if(this.$store.state.board.selectedCard === null) return ''
+    return this.$store.state.cards.dataCards[this.$store.state.board.selectedCard]['target'] ? 'target' : ''
   }
 
   private onClick(): void {
