e271a5b4a7d9f031f328a2a6544d1f9e69cd426e
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2022-09-15 23:29:02 +0200

    choix starter

--- src/components/interface/GameInterface.vue
+++ src/components/interface/GameInterface.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="game-interface" :style="{'background-image':'url(' + getBackground() + ')'}">
-</div>
+    <div class="game-interface" :style="{'background-image':'url(' + getBackground() + ')'}"></div>
+    <div class="starter-choice">
+        <div class="starters-wrapper">
+            <img class="pokemon-sprite bulba" :src="getStarter('007')">
+            <img class="pokemon-sprite bulba" :src="getStarter('004')">
+            <img class="pokemon-sprite bulba" :src="getStarter('001')">
+        </div>
+    </div>
 </template>
 
 <script>
@@ -17,6 +23,10 @@ export default {
         getBackground() {
             return require('./../../assets/img/interface/top_border.jpg')
         },
+
+        getStarter(id) {
+            return require(`@/assets/img/sprites/${this.$store.state.settings.pokemonSpritesExtension}/${id}.${this.$store.state.settings.pokemonSpritesExtension}`)
+        }
     }
 } 
 </script>
@@ -25,5 +35,28 @@ export default {
     width: 100%;
     height: 6%;
     background-position: bottom;
+}
+
+.starter-choice {
+    position: relative;
+    width: 100%;
+    height: 94%;
+
+    .starters-wrapper {
+        position: absolute;
+        display: flex;
+        justify-content: space-evenly;
+        top: 42%;
+        transform: translateX(17%);
+        width: 75%;
+    }
+
+    .pokemon-sprite {
+        display: block;
+
+        &.bulba {
+            width: 14%;
+        }
+    }
 }
 </style>
