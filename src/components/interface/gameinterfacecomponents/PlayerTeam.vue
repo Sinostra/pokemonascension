d0b610308eadb0b017382c2cfc52c2036884a8a1
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2022-10-10 10:49:16 +0200

    mise en place affichage barre de vie du pokémon

--- src/components/interface/gameinterfacecomponents/PlayerTeam.vue
+++ src/components/interface/gameinterfacecomponents/PlayerTeam.vue
@@ -1,13 +1,24 @@
 <template>
-    <div v-for="(pokemon, index) in $store.getters.getPlayerTeam"
-    :key="index"
-    class="team-member-wrapper"
-    >
-        <DisplayPokemon
-        :id="pokemon.id"
-        :width="10"
-        :displayHelpTooltip="false"
-        ></DisplayPokemon>
+    <div class="team-wrapper">
+        <div v-for="(pokemon, index) in $store.getters.getPlayerTeam"
+        :key="index"
+        class="team-member-wrapper"
+        >
+            <DisplayPokemon
+            :id="pokemon.id"
+            :width="100"
+            :displayHelpTooltip="false"
+            ></DisplayPokemon>
+
+            <div class="healthBar-infos-wrapper">
+                <div class="healthBar">
+                    <div class="currentHealth" :class="getHealthBarClass(index)" :style="{'width': getHealthBarPercent(index) + '%'}"></div>
+                    <div class="bottom-infos">
+                        <div class="healthAmount" :style="getFontSize(0.8)">{{getCurrentHealth(index)}}/{{getMaxHealth(index)}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     
 </template>
@@ -23,7 +34,31 @@ import DisplayCard from "../../card/DisplayCard.vue"
         DisplayCard
     }
 })
-export default class PlayerTeam extends Vue {}
+export default class PlayerTeam extends Vue {
+
+    public getMaxHealth(index) {
+        const id = this.$store.getters.getPlayerTeam[index]['id']
+        return this.$store.getters.getDex[id]['baseStats']['hp']
+    }
+
+    public getCurrentHealth(index) {
+        return this.$store.getters.getPlayerTeam[index]['remainingHp']
+    }
+
+    public getHealthBarPercent(index) {
+        return (this.getCurrentHealth(index) / this.getMaxHealth(index) * 100)
+    }
+
+    public getHealthBarClass(index) {
+        if(this.getHealthBarPercent(index) > 67) return 'green'
+        else if(this.getHealthBarPercent(index) > 33 && this.getHealthBarPercent(index) <= 67) return 'orange'
+        else return 'red'
+    }
+
+    public getFontSize(multiplier = 1): string {
+        return `font-size: ${(this.$store.state.settings.baseFontSize) * multiplier}px;`
+    }
+}
 </script>
 <style lang="scss" scoped>
 @import '../style/playerTeam.scss';
