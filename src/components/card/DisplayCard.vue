4e419683ac3ed805d74e252c2667c1c799c9660a
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2022-10-10 10:13:34 +0200

    affichage pokémon de l'équipe

---
+++ src/components/card/DisplayCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="card" :class="dataCard[id]['effect']['type']">
+    <div class="background-img" :style="{'background-image':`url(${background})`}"></div>
+    <div class="cost" :style="(getFontSize(1))">
+      <img :src="costBackground">
+      {{ dataCard[id]['cost'] }}
+    </div>
+    
+    <div class="name" :style="(getFontSize(0.7))">{{dataCard[id]['name']}}</div>
+
+    <div class="illustration" :style="{'background-image':'url(' + illustrationBackground + ')'}"></div>
+
+    <div class="category" :style="(getCategoryStyle(0.6))">{{dataCard[id]['category']}}</div>
+    
+    <div class="tooltip" :style="(getFontSize(0.8))">
+      <div v-html="dynamicToolTip"></div>
+      <div v-if="dataCard[id]['exhaust']" class="bold">Exhaust</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import dataCard from "@/store/constantData/cards/data-cards"
+
+@Options({
+  name: "Card",
+  props: {
+    id: String,
+  }
+})
+
+export default class DisplayCard extends Vue {
+  public id!: string
+
+  public dataCard = dataCard
+
+  get background() {
+    return require(`@/assets/img/cards/bords/${this.dataCard[this.id]['rarity']}2.png`)
+  }
+
+  get costBackground() {
+    return require(`@/assets/img/cards/bords/${this.dataCard[this.id]['rarity']}_round.png`)
+  }
+
+  get illustrationBackground(): string {
+    return require(`@/assets/img/cards/illustrations/${this.id}.jpg`)
+  }
+
+  get categoryBackground() {
+    return require(`@/assets/img/cards/${this.dataCard[this.id]['category']}_${this.dataCard[this.id]['rarity']}.png`)
+  }
+
+  get dynamicToolTip() {
+
+    if(!this.dataCard[this.id]['effect']['damage'] && !this.dataCard[this.id]['effect']['block']) {
+      return this.dataCard[this.id]['tooltip']
+    } 
+
+    return this.dataCard[this.id]['tooltip'].replace('§', this.dataCard[this.id]['effect']['damage']).replace('µ', this.dataCard[this.id]['effect']['block'])
+
+  }
+
+  getFontSize(multiplier = 1): string {
+    return `font-size: ${(this.$store.state.settings.baseFontSize) * multiplier}px;`
+  }
+
+  getCategoryStyle(multiplier = 1) {
+    const fontSize = this.getFontSize(multiplier)
+    const background = `background-image :url(${this.categoryBackground});`
+    return fontSize + background
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './style/card.scss';
+@import '../animations/cards/draw.scss';
+@import '../animations/cards/discard.scss';
+</style>
--- src/components/interface/gameinterfacecomponents/PlayerTeam.vue
+++ src/components/interface/gameinterfacecomponents/PlayerTeam.vue
@@ -1,11 +1,30 @@
 <template>
-    <div>Here is the team</div>
+    <div v-for="(pokemon, index) in $store.getters.getPlayerTeam"
+    :key="index"
+    class="team-member-wrapper"
+    >
+        <DisplayPokemon
+        :id="pokemon.id"
+        :width="10"
+        :displayHelpTooltip="false"
+        ></DisplayPokemon>
+    </div>
+    
 </template>
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import DisplayPokemon from "../../pokemon/DisplayPokemon.vue"
+import DisplayCard from "../../card/DisplayCard.vue"
 
 @Options({
     name: "PlayerTeam",
+    components: {
+        DisplayPokemon,
+        DisplayCard
+    }
 })
 export default class PlayerTeam extends Vue {}
 </script>
+<style lang="scss" scoped>
+@import '../style/playerTeam.scss';
+</style>
