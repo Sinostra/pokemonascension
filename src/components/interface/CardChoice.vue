839d1c75976411f696bb8fdde2019cb8c1968536
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2022-10-12 11:20:00 +0200

    mise en place affichage choix des cartes

--- src/components/interface/CardChoice.vue
+++ src/components/interface/CardChoice.vue
@@ -1,19 +1,41 @@
 <template>
-
+    <div class="proposed-cards-wrapper">
+        <div v-for="(card, index) in proposedCards" :key="index" class="proposed-card">
+            <DisplayCard :id="card"></DisplayCard>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
+import DisplayCard from "../card/DisplayCard.vue"
+import cloneDeep from "lodash.clonedeep"
 
 @Options({
     name: "CardChoice",
+    components: {
+        DisplayCard
+    }
 })
 export default class CardChoice extends Vue {
+
+    public proposedCardsAmount = 3
+
+    public proposedCards: string[] = []
+
     public mounted() {
-        const cardChoice = this.$store.getters.getActivePokemon.id
-        console.log(cardChoice)
+        const cardChoice = cloneDeep(this.$store.getters.getDex[this.$store.getters.getActivePokemon.id]['compatibleCards'])
+        cardChoice.sort(() => {
+            if(Math.random() > 0.5) return -1
+            else return 1
+        })
+
+        for(let i = 0; i < this.proposedCardsAmount; i++) {
+            this.proposedCards.push(cardChoice.shift())
+        }
     }
 }
 </script>
 <style lang="scss" scoped>
+@import './style/cardChoice.scss';
 </style>
